5431494246e417e3896ac686a4e5c98e924f18b1
IndraHardika <92752408+KZdra@users.noreply.github.com>
2023-11-30 21:17:41 +0700

    UI FOR NILAI

--- views/nilai/student.php
+++ views/nilai/student.php
@@ -3,7 +3,43 @@
 <div class="nisnilai"><strong><?php echo $_SESSION['nis']; ?></strong></div>
 <div class="namenilai"><strong><?php echo $_SESSION['name']; ?></strong></div>
 </div>
-
+<div class="judul">
+            <div class="judul-txt">
+                <h2>Mata Pelajaran:</h2>
+ </div>
+</div>
+           
 <div class="nilaisiswacont">
-    
+    <div class="nilaicard">
+        <div class="produktif-title">Produktif</div>
+        <div class="produktif-nilai"><h2>80</h2></div>
+    </div>
+    <div class="nilaicard">
+        <div class="ppkn-title">PPKN</div>
+        <div class="ppkn-nilai"><h2>80</h2></div>
+    </div>
+    <div class="nilaicard">
+        <div class="bsunda-title">Bahasa Sunda</div>
+        <div class="bsunda-nilai"><h2>80</h2></div>
+    </div>
+    <div class="nilaicard">
+        <div class="produktif-title">Produktif</div>
+        <div class="produktif-nilai"><h2>80</h2></div>
+    </div>
+    <div class="nilaicard">
+        <div class="produktif-title">Produktif</div>
+        <div class="produktif-nilai"><h2>80</h2></div>
+    </div>
+    <div class="nilaicard">
+        <div class="produktif-title">Produktif</div>
+        <div class="produktif-nilai"><h2>80</h2></div>
+    </div>
+    <div class="nilaicard">
+        <div class="produktif-title">Produktif</div>
+        <div class="produktif-nilai"><h2>80</h2></div>
+    </div>
+    <div class="nilaicard">
+        <div class="produktif-title">Produktif</div>
+        <div class="produktif-nilai"><h2>80</h2></div>
+    </div>
 </div>
